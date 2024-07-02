6468e2cecec0c23a7e4b3ccf137e534542766b21
JAIME <leoag1990@gmail.com>
2024-07-02 00:44:49 -0500

    primero

--- aichat/models.py
+++ aichat/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
-# Create your models here.
+class DatosUsuarios(AbstractUser):
+    email = models.EmailField(unique=True)
+    groups = models.ManyToManyField(Group, related_name='customuser_set')
+    user_permissions = models.ManyToManyField(Permission, related_name='customuser_set')
+
+class ChatMensaje(models.Model):
+    user = models.ForeignKey(DatosUsuarios, on_delete=models.CASCADE)
+    mensaje = models.TextField()
+    respuesta = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['timestamp']
--- chatbot_c4New/settings.py
+++ chatbot_c4New/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #aplicaciones propias
+    'aichat',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +132,5 @@ STATICFILES_DIR = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'aichat.DatosUsuarios'
